3ebe770f6055470f06267a163c5e5cedb316ed3d
Jérémy <jeremy.vaneste@free.fr>
2022-08-03 03:01:57 +0200

    edit tsconfig.json

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/header/Header";
+
+function App(): JSX.Element {
+  return (
+    <React.Fragment>
+      <Header></Header>
+    </React.Fragment>
+  );
+}
+
+export default App;
