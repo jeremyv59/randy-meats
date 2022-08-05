7ce9082031b35cac72fb524918cd74dff6b660ea
Jérémy <jeremy.vaneste@free.fr>
2022-08-05 17:35:13 +0200

    react icons installation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,8 +7,13 @@ const Header = (): JSX.Element => {
   return (
     <div className="full_container">
       <div className="container header_container">
-        <img className="logo" src={String(logo)} alt="logo"></img>
-        <a href="/menus">Mes menus</a>
+        <div className="logo_container">
+          <img className="logo" src={String(logo)} alt="logo"></img>
+          <h4>RandyMeats</h4>
+        </div>
+        <div className="item_container">
+          <a href="/menus">Mes menus</a>
+        </div>
       </div>
       {/* <div className="container_links"></div> */}
     </div>
