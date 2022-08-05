4d7fe2141ca846ef6eb79f9745b5037c62e42b49
Jérémy <jeremy.vaneste@free.fr>
2022-08-05 16:43:22 +0200

    header update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./header.css";
+
+const logo = require("../../assets/logo.png");
 
 const Header = (): JSX.Element => {
-  return <React.Fragment>Header</React.Fragment>;
+  return (
+    <div className="full_container">
+      <div className="container logo_container">
+        <img className="logo" src={String(logo)} alt="logo"></img>
+      </div>
+      <div className="container_links"></div>
+    </div>
+  );
 };
 
 export default Header;
