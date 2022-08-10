6952d0e39853791a5e4fb799644b05d85263c5c1
jérémy vaneste <jeremy.vaneste@free.fr>
2022-08-10 17:17:39 +0200

    MainContent component creation with css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header/Header";
+import MainContent from "./components/main_content/MainContent";
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
       <Header></Header>
+      <MainContent></MainContent>
     </React.Fragment>
   );
 }
---
+++ src/components/main_content/MainContent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./main_content.css";
+
+const MainContent = () => {
+  return (
+    <div className="container">
+      <h2>
+        Bienvenue sur RandyMeats, ici vous pourrez créer des menus en fonction
+        de vos goûts et les enregistrer afin de pouvoir les avoir sous la main
+      </h2>
+    </div>
+  );
+};
+
+export default MainContent;
