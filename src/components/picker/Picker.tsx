ab91f103527be702ff3785614d39e4ba0ce915b4
jérémy vaneste <jeremy.vaneste@free.fr>
2022-08-24 15:45:32 +0200

    add custom styles to react-select with types

--- src/components/picker/Picker.tsx
+++ src/components/picker/Picker.tsx
@@ -1,4 +1,50 @@
-import React from "react";
+import React, { CSSProperties } from "react";
+import "./picker.css";
+import Select, {
+  ControlProps,
+  CSSObjectWithLabel,
+  MenuProps,
+  OptionProps,
+  StylesConfig,
+} from "react-select";
+
+type MyOptionType = {
+  label: string;
+  value: string;
+};
+
+const options: MyOptionType[] = [
+  { value: "Très facile", label: "Très facile" },
+  { value: "facile", label: "facile" },
+  { value: "Difficile", label: "difficile" },
+  { value: "Très facile", label: "Très facile" },
+];
+
+const customStyles = {
+  menu: (provided: CSSObjectWithLabel, state: ControlProps) => ({
+    ...provided,
+    width: "100%",
+    color: "red",
+  }),
+  control: (provided: CSSObjectWithLabel, state: ControlProps) => ({
+    ...provided,
+    maxHeight: 36,
+    color: "#284b63",
+    border: "1px solid #284b63",
+    borderRadius: "0.5rem",
+    marginTop: "0.2rem",
+    boxShadow: state.isFocused ? "#284b63" : "none",
+    "&:hover": {
+      border: "1px solid #284b63",
+      boxShadow: "0px 0px 3px #284b63",
+    },
+  }),
+  option: (provided: CSSObjectWithLabel, state: ControlProps) => ({
+    ...provided,
+    backgroundColor: state.isFocused && "lightgray",
+    color: "#284b63",
+  }),
+};
 
 const Picker = () => {
   return <div>Picker</div>;
