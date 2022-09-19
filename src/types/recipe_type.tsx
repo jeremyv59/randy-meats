09ae2174aed631e47687f157f7817cb14b4a074c
jérémy vaneste <jeremy.vaneste@free.fr>
2022-09-19 15:03:26 +0200

    create types folder, recipe type & recipeData type

---
+++ src/types/recipe_type.tsx
@@ -0,0 +1,20 @@
+export interface Recipe {
+  name: string;
+  rate: number;
+  url: string;
+  ingredients: string[];
+  author: string;
+  images: string[];
+  steps: string[];
+  description: string;
+  difficulty: number;
+  budget: number;
+  tags: string[];
+  prepTime: number;
+  totalTime: number;
+  people: number;
+}
+
+export interface RecipeData {
+  recipes: Recipe[];
+}
